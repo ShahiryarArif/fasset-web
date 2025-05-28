cf145fc4e99630b063fa871736084224ddd592e0
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2025-05-28 16:25:36 +0500

    Header fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <main className="bg-[url(/images/banner.jpg)] h-screen w-full">
-        {/* <h1>FASSET</h1> */}
-      </main>
-      <h1>Hello</h1>
+      <section className="relative -z-1 bg-[url(/images/banner.jpg)] bg-cover h-screen w-full">
+        <h1 className="absolute bottom-3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-9xl font-bold">FASSET</h1>
+      </section>
+      <section>
+        <h1 className="h-screen">Hello</h1>
+      </section>
     </div>
   );
 }
